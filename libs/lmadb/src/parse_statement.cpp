b8310a61dd66f379baef6fc08d7e85a077919f71
Sam Kellett <samkellett@gmail.com>
2018-05-16 19:45:25 +0100

    tweak style in parse_statement.cpp

--- libs/lmadb/src/parse_statement.cpp
+++ libs/lmadb/src/parse_statement.cpp
@@ -7,20 +7,23 @@ namespace lmadb {
 using namespace lmadb::pdk;
 using namespace lmadb::pdk::literals;
 
-constexpr auto column_name = alpha;
-constexpr auto table_name = alpha;
+constexpr auto column_name
+  = alpha;
+constexpr auto table_name
+  = alpha;
 
-constexpr auto type_name =
-  "int8"_ilit / to<ast::type::int64>;
+constexpr auto type_name
+  = "int8"_ilit / to<ast::type::int64>;
 
-constexpr auto column_def =
-  (column_name > type_name) / construct<ast::column_def>;
+constexpr auto column_def
+  = (column_name > type_name) / construct<ast::column_def>;
 
-constexpr auto create_table_statement =
-  ("create"_ilit > "table"_ilit > table_name > "("_lit > +column_def > ")"_lit)
+constexpr auto create_table_statement
+  = ("create"_ilit > "table"_ilit > table_name > "("_lit > (column_def % ","_lit) > ")"_lit)
   / construct<ast::create_table>;
 
-constexpr auto sql_statement = create_table_statement > ";"_lit;
+constexpr auto sql_statement
+  = create_table_statement > ";"_lit;
 
 auto parse_statement(const std::string_view sql) -> std::optional<ast::sql_statement>
 {
